c2f0513f411d3e8bbe06f2d8d9befe0e0f46ffba
azhu8 <azhu8@MandyBear.dhcp.wustl.edu>
2023-03-02 15:18:35 -0600

    hi

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -13,10 +13,11 @@ public class Methods {
 	 * @param y2 y-coordinate of another point
 	 * @return the Euclidean distance between (x1,y1) and (x2,y2)
 	 */
+	
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
 		double distance = 0;
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
-		
+		distance = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 		return distance;
 	}
 
@@ -34,17 +35,20 @@ public class Methods {
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
-
+		StdDraw.setPenColor(0,109,219);
+		StdDraw.circle(x, y, radius*3.0/4.0);
 		
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
-
+		StdDraw.setPenColor(146,0,0);
+		StdDraw.circle(x, y, radius*1.0/2.0);
 		
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
-
+		StdDraw.setPenColor(255,255.109);
+		StdDraw.circle(x, y, radius*1.0/4.0);
 		
 	}
 
@@ -62,7 +66,17 @@ public class Methods {
 	public static String substituteAll(String source, char target, String replacement) {
 		String result = "";
 		// TODO: Finish this method
-		
+		char[] stringarray = source.toCharArray();
+		for (int i=0; i<source.length(); i++)
+		{
+			if (stringarray[i] == target)
+			{
+				result = result + replacement;
+			} 
+			else {
+				result = result + stringarray[i];
+			}
+		}
 		return result;
 	}
 
@@ -75,7 +89,10 @@ public class Methods {
 	public static int arraySum(int[] values) {
 		int sum = 0;
 		// FIXME: Compute the sum of the values in an array
-		
+		for (int i=0; i<values.length; i++)
+		{
+			sum=sum+values[i];
+		}
 		return sum;
 	}
 
@@ -87,9 +104,12 @@ public class Methods {
 	 * @return and array of size that's filled with value
 	 */
 	public static int[] filledArray(int length, int value) {
-		int[] values = null; // FIXME: Create an array of the appropriate size
+		int[] values = new int[length]; // FIXME: Create an array of the appropriate size
 		// TODO: Finish this method
-
+		for (int i=0; i<length; i++)
+		{
+			values[i] = value;
+		}
 		
 
 		return values;

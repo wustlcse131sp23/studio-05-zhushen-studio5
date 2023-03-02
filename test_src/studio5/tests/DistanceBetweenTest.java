0e755c7f1940a352bfadcd998dcf784b51cca67d
azhu8 <azhu8@MandyBear.dhcp.wustl.edu>
2023-03-02 15:18:52 -0600

    hi again

--- test_src/studio5/tests/DistanceBetweenTest.java
+++ test_src/studio5/tests/DistanceBetweenTest.java
@@ -61,7 +61,7 @@ public class DistanceBetweenTest {
 	public void testPointsD() {
 		// TODO: FIXME!  This test has an error
 		// Test that the distance between (0,0) and (30,40) is 50.0 (order of parameters matters!)
-		assertEquals(50.0, Methods.distanceBetween(0.0, 30.0, 0.0, 40.0), 1e-5);
+		assertEquals(50.0, Methods.distanceBetween(0.0, 0.0, 30.0, 40.0), 1e-5);
 	}
 
 }
